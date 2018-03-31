1d33aeae21b79caa4b7ba301dbbc11cd0654c9bd
Andriy Ivashchenko <andr.ivas12@gmail.com>
2018-03-31 18:57:53 +0300

    new test without err

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,6 +1,6 @@
 describe('initial test', function () {
   it('should true to be true', function () {
-    expect(true).to.be.false;
+    expect(true).to.be.true;
   });
   it.skip('postpone your assertion', () => {
   });
