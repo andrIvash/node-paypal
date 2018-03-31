eb71e520dd22b1b49671b1fec62125458da5229a
Andriy Ivashchenko <andr.ivas12@gmail.com>
2018-03-31 18:47:26 +0300

    new test

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,6 +1,6 @@
 describe('initial test', function () {
   it('should true to be true', function () {
-    expect(true).to.be.true;
+    expect(true).to.be.false;
   });
   it.skip('postpone your assertion', () => {
   });
